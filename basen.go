d81c089b1ca5be5779a30bf2b8c532b12043bd8e
Matt Clendenen <clendenen.matthew@gmail.com>
2017-12-24 00:40:57 -0500

    add any base - no error validation

--- basen.go
+++ basen.go
@@ -1,25 +1,25 @@
 package basen
 
-import "fmt"
+// import "fmt"
 
 type BaseN struct {
     Charset []byte
-    BaseNDec map[string]int
-    DecBaseN map[int]string
+    BaseNDec map[byte]int
+    DecBaseN map[int]byte
     Base int
 }
 
 func NewBaseN(charset []byte, base int) (*BaseN) {
-    basendec := make(map[string]int)
-    decbasen := make(map[int]string)
+    basendec := make(map[byte]int)
+    decbasen := make(map[int]byte)
 
     for pos, char := range charset {
-        basendec[string(char)] = pos
-        decbasen[pos] = string(char)
+        basendec[char] = pos
+        decbasen[pos] = char
     }
 
     return &BaseN {
-        Charset: charset
+        Charset: charset,
         Base: base,
         BaseNDec: basendec,
         DecBaseN: decbasen,
@@ -27,10 +27,11 @@ func NewBaseN(charset []byte, base int) (*BaseN) {
 }
 
 // add byte arrays of base n
-func (basen *BaseN) Add(augend []byte, addend []byte) {
+func (basen *BaseN) Add(augend []byte, addend []byte) []byte {
+    // first vars
     var long []byte
     var short []byte
-    // designate long / short quantity
+    // designate long / short quantities
     // reverse byte arrays
     if len(augend) > len(addend) {
         long = reverse(augend)
@@ -40,15 +41,44 @@ func (basen *BaseN) Add(augend []byte, addend []byte) {
         short = reverse(augend)
     }
 
-    fmt.Printf("%s + %s", long, short)
+    // second vars
+    resultant := make([]byte, len(long))
+    remainder := 0
+    carry := 0
 
-    for dec, char := range basen.DecBaseN {
-        fmt.Print(dec)
-        fmt.Println(" - " + char)
+    var sdec int
+
+    // iterate through long by pos and character
+    for lpos, lchar := range long {
+        ldec := basen.BaseNDec[lchar]
+        if lpos >= len(short) {
+            sdec = 0
+        } else {
+            sdec = basen.BaseNDec[short[lpos]]
+        }
+
+        singleSum := ldec + sdec + carry
+        remainder = 0
+        carry = 0
+        if singleSum >= basen.Base {
+            carry = 1
+            remainder = singleSum - basen.Base
+            resultant[lpos] = basen.DecBaseN[remainder]
+        } else {
+            resultant[lpos] = basen.DecBaseN[singleSum]
+        }
     }
 
-    // for dec := basen.Base - 1
-
+    if carry == 1 {
+        r := make([]byte, len(resultant) + 1)
+        for pos, char := range resultant {
+            r[pos] = char
+        }
+        r[len(resultant)] = basen.DecBaseN[1]
+        resultant = r
+    }
+    resultant = reverse(resultant)
+    return resultant
 }
 
 // reverse byte array
@@ -62,60 +92,3 @@ func reverse(a []byte) []byte {
     }
     return reversed
 }
-
-// func (basen *BaseN) Add(augend []byte, addend []byte) []byte {
-// func (basen *BaseN) Add() {
-    // var long []byte
-    // var short []byte
-    //
-    // if len(augend) > len(addend) {
-    //     long = augend
-    //     short = addend
-    // } else {
-    //     long = addend
-    //     short = augend
-    // }
-    // resultant := make([]byte, len(long))
-    // remainder := 0
-    // carry := 0
-    // var shortDec int
-
-    // fmt.Printf("%+v", basen)
-    // for dec := basen.Base - 1; dec >= 0; dec-- {
-    //     fmt.Println(dec)
-    // }
-
-
-
-    // //
-    // // fmt.Printf("Long: %s\nShort: %s\n", long, short)
-    // for pos, char := range long {
-    //     longDec := base62dec[char]
-    //     if pos >= len(short) {
-    //         shortDec = 0
-    //     } else {
-    //         shortDec = base62dec[short[pos]]
-    //     }
-    //
-    //     singleSum := longDec + shortDec + carry
-    //     remainder = 0
-    //     carry = 0
-    //     if singleSum >= 62 {
-    //         carry = 1
-    //         remainder = singleSum - 62
-    //         resultant[pos] = decbase62[remainder]
-    //     } else {
-    //         resultant[pos] = decbase62[singleSum]
-    //     }
-    // }
-    // if carry == 1 {
-    //     r := make([]byte, len(resultant) + 1)
-    //     for pos, char := range resultant {
-    //         r[pos] = char
-    //     }
-    //     r[len(resultant)] = decbase62[1]
-    //     resultant = r
-    // }
-    // resultant = reverse(resultant)
-    // return resultant
-// }
